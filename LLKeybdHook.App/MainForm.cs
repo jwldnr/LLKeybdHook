9aa35950f14a3c45338936b794da0e51a3f87eb6
Jean Alexander Woldner <alexander@woldner.io>
2018-01-14 17:36:48 +0100

    get rid of timers, use task instead

--- LLKeybdHook.App/MainForm.cs
+++ LLKeybdHook.App/MainForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using WindowsInput;
 using WindowsInput.Native;
@@ -7,26 +8,16 @@ namespace jwldnr.LLKeybdHook.App
 {
     public partial class MainForm : Form
     {
-        private bool CooldownIsReady => false == _cooldownTimer.Enabled || false == _hasCooldown;
-
-        private readonly Timer _cooldownTimer = new Timer();
         private readonly InputSimulator _inputSimulator = new InputSimulator();
         private readonly KeyboardHook _keyboardHook = new KeyboardHook();
 
-        private bool _hasCooldown;
+        private bool _isOnCooldown;
 
         public MainForm()
         {
             InitializeComponent();
-            InitializeCooldownTimer();
 
             InstallHook();
-        }
-
-        private void InitializeCooldownTimer()
-        {
-            _cooldownTimer.Interval = 2000;
-            _cooldownTimer.Tick += OnTimerTick;
         }
 
         private void InstallHook()
@@ -34,15 +25,17 @@ namespace jwldnr.LLKeybdHook.App
             _keyboardHook.Install();
 
             _keyboardHook.KeyDown += OnKeyDown;
-            _keyboardHook.KeyUp += OnKeyUp;
 
             Application.ApplicationExit += OnApplicationExit;
         }
 
         private void Log(string message)
         {
-            EventLog.AppendText($"{message + Environment.NewLine}");
-            EventLog.ScrollToCaret();
+            EventLog.BeginInvoke(new Action(() =>
+            {
+                EventLog.AppendText($"{message + Environment.NewLine}");
+                EventLog.ScrollToCaret();
+            }));
         }
 
         private void OnApplicationExit(object sender, EventArgs e)
@@ -55,48 +48,32 @@ namespace jwldnr.LLKeybdHook.App
             if (Keys.W != e.KeyCode || e.IsInjected)
                 return;
 
-            //if (e.IsInjected)
-            //    e.Handled = true;
-
-            if (!CooldownIsReady)
+            if (_isOnCooldown)
                 return;
 
             e.SuppressKeyPress = true;
             e.Handled = true;
 
-            _cooldownTimer.Enabled = true;
-            _hasCooldown = true;
+            _isOnCooldown = true;
 
-            _inputSimulator.Keyboard
-                .KeyPress(VirtualKeyCode.VK_Q)
-                .KeyPress(VirtualKeyCode.VK_W);
+            Task.Run(async () =>
+            {
+                _inputSimulator.Keyboard
+                    .KeyPress(VirtualKeyCode.VK_Q)
+                    .KeyPress(VirtualKeyCode.VK_W);
 
-            //Log($"{e.KeyCode} down");
-        }
+                await Task.Delay(2000)
+                    .ConfigureAwait(false);
 
-        private void OnKeyUp(object sender, KeyEventArgsEx e)
-        {
-            //if (Keys.W != e.KeyCode)
-            //    return;
+                Log("OnKeyDown callback");
 
-            //if (e.IsInjected)
-            //    e.Handled = true;
-
-            //Log($"{e.KeyCode} up");
-        }
-
-        private void OnTimerTick(object sender, EventArgs e)
-        {
-            Log(nameof(OnTimerTick));
-
-            _cooldownTimer.Enabled = false;
-            _hasCooldown = false;
+                _isOnCooldown = false;
+            });
         }
 
         private void UninstallHook()
         {
             _keyboardHook.KeyDown -= OnKeyDown;
-            _keyboardHook.KeyUp -= OnKeyUp;
 
             _keyboardHook.Dispose();
         }
